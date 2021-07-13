8f94888c34534b8c259da633c292c1b10fc1d234
BL147 <samoff123456@gmail.com>
2021-07-13 18:55:43 +0530

    Restaurants code
    Restaurant api which is very simple

---
+++ Restaurant.py
@@ -0,0 +1,18 @@
+# Restaurants billing and all
+
+name = input("Enter your name:")
+
+print("Welcoem what would yout like to eat Mr/Mrs" + name)
+
+menu_order = input("pizza\nburger\nmendu vada\nWhat would you like (type the same as what it is written)")
+
+menu = ["Pizza", "Burger", "Mendu vada"]
+
+if (menu_order == "pizza"):
+    print("Here is your" + menu[0])
+elif(menu_order == "burger"):
+    print("Here is your" + menu[1])
+elif (menu_order == "mendu vada"):
+    print("Here is your" + menu[2])
+else:
+    print("wrong spelling pls try again")
