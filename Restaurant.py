7ad2b585bc6ed22f9c351c345bb5c17127a78ad5
BL147 <samoff123456@gmail.com>
2021-07-13 19:04:06 +0530

    Bakliwal stuff added

--- Restaurant.py
+++ Restaurant.py
@@ -8,11 +8,11 @@ menu_order = input("pizza\nburger\nmendu vada\nWhat would you like (type the sam
 
 menu = ["Pizza", "Burger", "Mendu vada"]
 
-if (menu_order == "pizza"):
+if menu_order == "pizza":
     print("Here is your" + menu[0])
-elif(menu_order == "burger"):
+elif menu_order == "burger":
     print("Here is your" + menu[1])
-elif (menu_order == "mendu vada"):
+elif menu_order == "mendu vada":
     print("Here is your" + menu[2])
 else:
     print("wrong spelling pls try again")
