46b8dbeb5bf4c9c3aaf2786a1b59b40b01ea6c13
BL147 <samoff123456@gmail.com>
2021-07-13 19:18:10 +0530

    Minor changes

--- Bakliwal/Bakliwal work.py
+++ Bakliwal/Bakliwal work.py
@@ -17,21 +17,21 @@ b = word[1]
 c = word[2]
 d = word[3]
 
-print(str(a)+""+str(b)+""+str(c)+""+str(d))
+print(str(a)+" "+str(b)+" "+str(c)+" "+str(d))
 
 a1 = ord(a)
 b1 = ord(b)
 c1 = ord(c)
 d1 = ord(d)
 
-print(str(a1)+""+str(b1)+""+str(c1)+""+str(d1))
+print(str(a1)+" "+str(b1)+" "+str(c1)+" "+str(d1))
 
 a1 = a1+1
 b1 = b1+1
 c1 = c1+1
 d1 = d1+1
 
-print(str(a1)+""+str(b1)+""+str(c1)+""+str(d1))
+print(str(a1)+" "+str(b1)+" "+str(c1)+" "+str(d1))
 
 
 a2 = chr(a1)
