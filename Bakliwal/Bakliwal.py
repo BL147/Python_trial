bb5ac79a31dfb821f5e8967b491cc108cdd4c731
BL147 <samoff123456@gmail.com>
2021-11-09 20:04:22 +0530

    Major changes
    ur mom

---
+++ Bakliwal/Bakliwal.py
@@ -0,0 +1,10 @@
+'''
+
+1. take input from user in a loop
+2. inpulist---> Contains all the numbers
+3. sortedlist---> empty
+4. Find the min element in the inplist
+5. Add to the sorted list and remove from inplist
+6.
+'''
+
